8c33e2a99ee3a2696405537f91197bd07b1b26bd
sayanaberikbai-pixel <sayana.berikbai@narxoz.kz>
2026-05-26 00:18:25 +0500

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from enum import Enum
 
-# 1. Сөздік (Enum) анықтау
+
 class Status(str, Enum):
     WORKING = "working"
     BROKEN  = "broken"
@@ -9,7 +9,7 @@ class Status(str, Enum):
     RETIRED = "retired"
 
 
-# 2. Классты анықтау
+
 @dataclass
 class Device:
     pc_id:  str
@@ -35,20 +35,20 @@ class Device:
 if __name__ == "__main__":
 
 
-    # 1. Дұрыс нысан құру
+   
     try:
         dev1 = Device(pc_id="PC-101-01", status="working", room="101")
         print(f"Құрылғы сәтті құрылды: {dev1.pc_id}, Күйі: {dev1.status.value}, Жарамды: {dev1.is_ok()}")
     except ValueError as e:
         print(f"Қате: {e}")
 
-    # 2. Қате мәліметпен тексеру (pc_id бос болғанда)
+   
     try:
         dev2 = Device(pc_id="   ", status="broken", room="102")
     except ValueError as e:
         print(f"Күтілген қате (pc_id бос): {e}")
 
-    # 3. Қате мәліметпен тексеру (room бос болғанда)
+    
     try:
         dev3 = Device(pc_id="PC-102-05", status="repair", room="")
     except ValueError as e:
